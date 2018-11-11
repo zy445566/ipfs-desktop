01111700fc3a5ca221dfe072d8efe9864a8c1ec2
zy445566 <zycy@vip.qq.com>
2018-11-11 21:31:43 +0800

    fix show like command

--- util/showLikeCommand.js
+++ util/showLikeCommand.js
@@ -1,17 +1,29 @@
-module.exports = function showLikeCommand (dom,text,interval=100) {
-    let index = 0;
-    let timeInterval = setInterval(function(){
-        if (text[index]==' ') {
-            if (index+1<text.length) {
-                dom.innerText = dom.innerText+(text[index]+text[index+1]);
+module.exports = function showLikeCommand (dom,text,repeat=false,backOrgin=false,interval=100) {
+    return new Promise((reslove,reject)=>{
+        let index = 0;
+        let orginText = dom.innerText;
+        let timeInterval = setInterval(function(){
+            if (text[index]==' ') {
+                if (index+1<text.length) {
+                    dom.innerText = dom.innerText+(text[index]+text[index+1]);
+                }
+                index++;
+            } else {
+                dom.innerText = dom.innerText+text[index];  
             }
             index++;
-        } else {
-            dom.innerText = dom.innerText+text[index];  
-        }
-        index++;
-        if (index>=text.length) {
-            clearInterval(timeInterval);
-        }
-    },interval);
+            if (index>=text.length) {
+                if (repeat) {
+                    dom.innerText=orginText;
+                    index=0;
+                } else {
+                    if (backOrgin) {
+                        dom.innerText=orginText;
+                    }
+                    clearInterval(timeInterval);
+                    reslove(true)
+                }
+            }
+        },interval);
+    });
 }

572f1a5c67d7f4df3b2891e9b57303919c5193d4
ZhangYuan <zyct@vip.qq.com>
2018-11-12 15:23:59 +0800

    add electron pack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow,ipcMain } = require('electron')
 
 
 
-let dev = process.argv.length==3 && process.argv[2]=='dev';
+let dev = process.argv.length==3 && process.argv[2]=='dev' || false;// || true 只是在非必须下方便调试
 
 let initWin;
 let mainWin;
--- util/getAppInfo.js
+++ util/getAppInfo.js
@@ -1,6 +1,6 @@
 const path = require('path');
 let app_dir = path.dirname(__dirname);
-let ipfs_dir = path.join(app_dir,'ipfs',process.platform);
+let ipfs_dir = path.join(app_dir,'ipfs',process.platform).replace("app.asar","app.asar.unpacked");
 let ipfs_file_path;
 if (process.platform=='win32') {
     ipfs_file_path = path.join(ipfs_dir,'ipfs.exe')

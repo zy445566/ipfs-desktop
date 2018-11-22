8ac5658a640a14211f52818fb1735edc495ac55e
ZhangYuan <zyct@vip.qq.com>
2018-11-22 11:38:21 +0800

    fix mac pack can't copy and paste

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const { app, BrowserWindow,ipcMain } = require('electron')
+const { app, BrowserWindow,ipcMain, Menu, MenuItem } = require('electron')
 
 let dev = process.argv.length==3 && process.argv[2]=='dev' || false;// || true 只是在非必须下方便调试
 
 let initWin;
 let mainWin;
 
-function createWindow () {   
+function createWindow () {
+  if (process.platform === 'darwin') {
+    const template = [
+      {
+        label: "Edit",
+        submenu: [
+          {
+            label: 'Copy',
+            accelerator: 'CmdOrCtrl+C',
+            role: 'copy'
+          },
+          {
+            label: 'Paste',
+            accelerator: 'CmdOrCtrl+V',
+            role: 'paste'
+          }
+        ]
+      }
+    ];
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+  }
   initWin = new BrowserWindow({ width: 400, height: 250 })
   initWin.loadFile('view/index.html');
   if (dev) {
